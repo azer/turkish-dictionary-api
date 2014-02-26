664991488f54308726b0790df8ed6f29174a6cdc
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-26 15:39:48 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## turkish-dictionary-api
+
+API Server for the official Turkish-to-turkish dictionary by TDK
+
+## Install
+
+```bash
+$ npm install turkish-dictionary-api
+```
+
+## Usage
+
+Start the server:
+
+```bash
+$ turkish-dictionary-api -p 8001
+```
+
+See all options:
+
+```bash
+$ turkish-dictionary-api -h
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+var debug = require("debug")('turkish-synonyms-api');
+var circle = require("circle");
+var scrape = require("scrape-url");
+var strip = require("strip");
+var memoize = require('memoize-with-leveldb')('./data-dictionary');
+var query = memoize(pull, -1);
+var url = 'http://www.tdk.gov.tr/index.php?option=com_gts&arama=gts';
+
+module.exports = circle({
+  '/:word': definitions
+});
+
+function definitions (reply, match) {
+  query(match.params.word, reply);
+}
+
+function pull (word, callback) {
+  scrape.post({ url: url, form: { kelime: word } }, '#hor-minimalist-a td', function (error, match) {
+    if (error) return callback(error);
+    if (match.length == 0) return callback(undefined, []);
+    callback(undefined, match.map(parse));
+  });
+}
+
+function parse (input) {
+  var lines = input.html().replace(/^\s+\d+\.\s*/, '').split(/<br\s*\/?>/);
+  var meaning = lines[0].split(/<\/?i>\s*/);
+  var result = {};
+
+  var type, definition;
+  var example = strip(lines[1]);
+
+  if (meaning.length == 3) {
+    definition = meaning[2];
+    type = meaning[1];
+  } else {
+    definition = meaning[meaning.length - 1];
+  }
+
+  example && (result.example = example);
+  type && (result.type = type);
+  result.definition = definition;
+
+  return result;
+}
